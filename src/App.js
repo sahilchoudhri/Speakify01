77f6581671914c8796b205115f103395660d08ca
sahilchoudhri <saahil999683@gmail.com>
2024-03-20 13:34:24 +0530

    alpha

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import useClipboard from 'react-use-clipboard';
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
+
 
 
 
@@ -9,8 +12,11 @@ function App() {
 
   const[textToCopy , setTextToCopy] = useState();
 
-  const[isCopied , setCopied] = useClipboard(textToCopy , "trascript");
+  const[isCopied , setCopied] = useClipboard(textToCopy , {
+    successDuration: 1000
+  });
 
+  const [darkMode, setDarkMode] = useState(false);
   const startListening = () => 
   {
      SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
@@ -21,31 +27,38 @@ function App() {
   {
     return null;
   }
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="App">
+    
+      <div className= {`container ${darkMode ? 'dark-mode' : ''}`}>
 
-      <div className='container '>
+        <h2  >SPEEKIFY</h2>
+        {/* <p> A React Hook that converts speech from your microphone into text and makes it available to your componets</p> */}
+        <p>Experience the power of speech-to-text conversion with our cutting-edge ReactJS project. Simply speak into your microphone and watch as your words are transformed into text in real-time. Whether you're dictating notes, transcribing interviews, or simply exploring the possibilities of voice technology, our project makes it easy and intuitive. Get started now and unlock a new dimension of productivity!</p>
 
-        <h2 >Speech To Text Converter Project</h2>
-        <p> A React Hook that converts speech from your microphone into text and makes it available to your componets</p>
 
-        <div className='main-content' onClick={() => setTextToCopy(transcript)}>
+        <div className='main-content' onClick={() => setTextToCopy(transcript)} >
            {transcript} 
         </div>
 
         <div className='btn-style'>
 
         <button onClick={setCopied}>
-           Was it copied? (isCopied ? "Yes!👍🏻" : "No 👎🏻") </button>
-          <button onClick={startListening}> Start Listening </button>
-          <button onClick={SpeechRecognition.stopListening}> Stop Listening </button>
-      
+          {isCopied ? 'Copied!' : 'Copy to clipboard'}
+          </button>
+           <button onClick={startListening}>Start Listening</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
         </div>
-{/* /snjshisuh */}
+
+        <div className='dark-mode-toggle' onClick={toggleDarkMode}>
+        {
+          darkMode ? <MdOutlineLightMode background-color='#333' text='#fff' />  : <MdOutlineDarkMode background-color= 'fff' text='#333' />
+        }
+        </div>
       </div>
-
-
-    </div>
   );
 }
 
